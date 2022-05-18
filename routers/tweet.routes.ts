d43708a85e8d4df12e9887cdb751ede0864960bf
Aimar <aimaroribevigo@gmail.com>
2022-05-17 20:41:06 -0500

    Rutas leer tweets y eliminar tweets agregados

--- routers/tweet.routes.ts
+++ routers/tweet.routes.ts
@@ -38,5 +38,78 @@ router.post('/', async (req, res) => {
   }
 })
 
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get('/yourOrOtherPersonTweets/', async (req, res) => {
+  const otherPersonId = String(req.query.idPerson)
+  const yourId = getUserIdByToken(String(req.headers.authorization).split(' ')[1])
+
+  try {
+    let tweets: any[]
+
+    if (otherPersonId === '') {
+      tweets = await TweetModel.find({ userId: yourId }).sort({ date: -1 })
+    } else {
+      tweets = await TweetModel.find({ userId: otherPersonId }).sort({ date: -1 })
+    }
+
+    return res.status(200).json({ message: 'Tweet o Tweets encontrados', tweets: tweets })
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    let message = `Error: ${error}`
+    let status = 500
+
+    if (error instanceof ValidateError) {
+      message = error.getParameter()
+      status = 400
+    }
+
+    return res.status(status).json({ message: message })
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get('/allTweets/', async (req, res) => {
+  const userId = getUserIdByToken(String(req.headers.authorization).split(' ')[1])
+  console.log(userId)
+
+  try {
+    const tweets: any[] = await TweetModel.find().sort({ date: -1 })
+
+    return res.status(200).json({ message: 'Tweet o Tweets encontrados', tweets: tweets })
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    let message = `Error: ${error}`
+    let status = 500
+
+    if (error instanceof ValidateError) {
+      message = error.getParameter()
+      status = 400
+    }
+
+    return res.status(status).json({ message: message })
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get('/deleteTweet/', async (req, res) => {
+  try {
+    const TweetId = String(req.query.id)
+    const resultado = await TweetModel.findByIdAndDelete(TweetId)
+    console.log(resultado)
+    return res.status(200).json({ message: 'Tweet eliminado correctamente' })
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    let message = `Error: ${error}`
+    let status = 500
+
+    if (error instanceof ValidateError) {
+      message = error.getParameter()
+      status = 400
+    }
+
+    return res.status(status).json({ message: message })
+  }
+})
+
 /** Contains the tweet paths */
 export default router
