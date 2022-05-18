6ddaa5abd9f60bfe4603c8aa2053649973751c77
ream <ream_tp@outlook.com>
2022-05-18 12:23:20 -0500

    Se agrego la capacidad obtener id de un token puro o un token con la palabra Bearer

--- auth/test/jwt.test.ts
+++ auth/test/jwt.test.ts
@@ -8,4 +8,8 @@ describe('JWT Test', () => {
   test('Get User Id By Token Test', () => {
     expect(getUserIdByToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyN2RmMzZiZWEzOWI4NWI1YjBhZDNjZCIsImlhdCI6MTY1MjQ1NzAwMCwiZXhwIjoxODEwMjQ1MDAwfQ.WF1dGXyzYeBUMc4Vn5A1N3QziBP5BiucqceC8H0lrhA')).toBe('627df36bea39b85b5b0ad3cd')
   })
+
+  test('Get User Id By Bearer Token Test', () => {
+    expect(getUserIdByToken('Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyN2RmMzZiZWEzOWI4NWI1YjBhZDNjZCIsImlhdCI6MTY1MjQ1NzAwMCwiZXhwIjoxODEwMjQ1MDAwfQ.WF1dGXyzYeBUMc4Vn5A1N3QziBP5BiucqceC8H0lrhA')).toBe('627df36bea39b85b5b0ad3cd')
+  })
 })
