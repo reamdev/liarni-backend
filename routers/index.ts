2e47004f530b5893115aaa8f4a4c6f5a995dbb79
Aimar <aimaroribevigo@gmail.com>
2022-05-18 20:38:55 -0500

    Se agrego el archivo relations.routes.ts, como tambien alta y baja relacion

--- routers/index.ts
+++ routers/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthRouter } from './auth.routes'
 export { default as UserRouter } from './user.routes'
 export { default as TweetRouter } from './tweet.routes'
+export { default as RelationRouter } from './relations.routes'
